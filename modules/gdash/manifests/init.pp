21e3a1cf1f5aa72228e8148a53cbad33f5ae3656
Kris Buytaert <github@krisbuytaert.be>
2011-12-18 16:01:01 +0100

    "Working" gdash

--- modules/gdash/manifests/init.pp
+++ modules/gdash/manifests/init.pp
@@ -16,18 +16,37 @@ class gdash {
  include passenger 
  package {"rubygem-sinatra": ensure => present }
 
+ # package {"bundler": ensure => 
 
- file {"/etc/httpd/conf.d/gdash.conf":
-  ensure  => 'file',
-  group   => '0',
-  mode    => '644',
-  owner   => '0',
-  source => "puppet:///gdash/gdash.conf",
-  notify => Service['httpd'];
+
+ # Build rpmn 's for .. include them in repo 
+ # bundler
+ # rack 1.3.4
+ # rack-protecion 1.1.4 
+ # tilt 1.3.3
+ # sinatra 1.3.1 
+
+
+
+ file {
+	"/etc/httpd/conf.d/gdash.conf":
+		ensure  => 'file',
+		group   => '0',
+		mode    => '644',
+		owner   => '0',
+		source => "puppet:///gdash/gdash.conf",
+ 		notify => Service['httpd'];
+	"/vagrant/other/gdash/config/gdash.yaml": 
+		source => "puppet:///gdash/gdash.yaml",
+		group   => '0',
+		owner   => '0';
 }
 
+# Introduce  siteroot variable for this .
+# Probably package gDash too :) 
 
 
 
-}
+
+} 
 
