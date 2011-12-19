760a1f0e5c311dd818a980a1b91fbe46163ac0e7
Kris Buytaert <github@krisbuytaert.be>
2011-12-19 19:46:53 +0100

    Adding empty moduls

---
+++ modules/jmxtrans/manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: jmxtrans
+#
+# This module manages jmxtrans
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class jmxtrans {
+
+
+}
