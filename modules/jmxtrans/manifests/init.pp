a1703d8dba98d9bf728e41c85a22d4b1e2789ff4
Kris Buytaert <github@krisbuytaert.be>
2011-12-21 21:11:41 +0100

    Jmxtrans development

--- modules/jmxtrans/manifests/init.pp
+++ modules/jmxtrans/manifests/init.pp
@@ -14,27 +14,21 @@
 class jmxtrans {
 
 
-	package {"jmxtrans": 
-		ensure => present;} 
-
-
-	service {"jmxtrans": 
-		ensure => running;
-	}
-
-
-			
+  package {"jmxtrans": 
+    ensure => present;} 
+  service {"jmxtrans": 
+    ensure => running; }
 
 }
 
-class jmxtrans::example {
+  class jmxtrans::example {
 
-file { '/var/lib/jmxtrans/localhost.json':
-  group   => '0',
-  mode    => '644',
-  owner   => '0',
-  source  => "puppet:///jmxtrans/localhost.json";
-}
+  file { '/var/lib/jmxtrans/localhost.json':
+    group   => '0',
+    mode    => '644',
+    owner   => '0',
+    source  => "puppet:///jmxtrans/localhost.json";
+  }
 
 }
 
