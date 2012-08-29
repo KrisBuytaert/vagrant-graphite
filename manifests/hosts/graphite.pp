f3c1467c8f7da31daf314a44d38ec3ccc54ebe70
Kris Buytaert <sdog@inuits.be>
2012-08-29 17:40:45 +0200

    Added tmpwatch stuff

--- manifests/hosts/graphite.pp
+++ manifests/hosts/graphite.pp
@@ -5,7 +5,22 @@ node 'default' {
 
   include my-repos
   include my-graphite
+  include apache
+  apache::listen{'80':}
+  apache::namevhost{'80':}
+  include passenger
+  class {'gdash':
+    graphitehost => '10.42.42.13'
+  }
 
+  tmpwatch::cleanup {'jmxtrans':
+    path => '/var/lib/jmxtrans',
+    age  => '5d',
+  }
+  tmpwatch::cleanup {'carbon':
+    path => '/var/lib/carbon',
+    age  => '5d',
+  }
 
 
 }
